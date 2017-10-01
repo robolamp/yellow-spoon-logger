e25d850f3450eb590afa3394c6a3c747f9ed8c2c
robolamp <redslowj@gmail.com>
2017-10-01 13:43:58 +0300

    Initial commit

---
+++ yespoologger.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python3
+
+import time
+import os
+import argparse
+
+from datetime import datetime, timedelta
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Server(BaseHTTPRequestHandler):
+    def _update_logfile(self, ts=time.time(), 
+                              time=datetime.now().strftime("%d %b %Y %H:%M:%S.%f"),
+                              whois="unknown:unknown",
+                              data=''):
+        
+        self._logheader = "ts,datetime,user,data"
+        self._logfilepath = ".post.log"
+        
+        if not os.path.exists(self._logfilepath):
+            file = open(self._logfilepath, 'w+')
+            file.write(self._logheader + "\n")
+            file.close()
+
+        logstring = '{},{},{},{}'.format(str(ts), time, whois, str(data))
+        file = open(self._logfilepath, 'a')
+        file.write(logstring + "\n")
+        file.close()
+        
+
+    def do_POST(self):
+        post_data = self.rfile.read(int(self.headers['Content-Length']))
+        post_sender = ":".join([str(n) for n in self.client_address])
+            
+        self._update_logfile(
+            whois = post_sender,
+            data = post_data.decode('ascii')
+        )
+
+        self.send_response(200, message="OK")
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        
+class Logger(object):
+    def __init__(self, port=9090):
+        self._port = port
+    
+    def run(self):
+        server_address = ('', self._port)
+        server = HTTPServer(server_address, Server)
+        start_time = time.time()
+        print('Starting server')
+        try:
+            server.serve_forever()
+        
+        except OSError as e:
+            print(e)
+            
+        except KeyboardInterrupt:
+            pass
+        
+        server.shutdown()
+        dtime = time.time() - start_time
+        dtime_str = timedelta(seconds=dtime)
+        
+        print('Server is offline')
+        print('Uptime is {}'.format(dtime_str))
+        
+        
+if __name__ == '__main__':
+    p = argparse.ArgumentParser(description='POST HTTP Requests logger')
+    p.add_argument('-P', '--port', type=int, help='port')  
+    args = p.parse_args()
+    logger = Logger(port=args.port)
+    logger.run()
