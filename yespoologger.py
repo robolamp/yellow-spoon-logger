2805660316f2c9d879afbb273cd057c7b50b42f5
robolamp <redslowj@gmail.com>
2017-10-01 19:59:04 +0300

    typo fixed

--- yespoologger.py
+++ yespoologger.py
@@ -33,8 +33,8 @@ class Server(BaseHTTPRequestHandler):
         post_sender = ":".join([str(n) for n in self.client_address])
             
         self._update_logfile(
-            ts=time.time()
-            time=datetime.now().strftime("%d %b %Y %H:%M:%S.%f")
+            ts=time.time(),
+            time=datetime.now().strftime("%d %b %Y %H:%M:%S.%f"),
             whois = post_sender,
             data = post_data.decode('ascii')
         )
